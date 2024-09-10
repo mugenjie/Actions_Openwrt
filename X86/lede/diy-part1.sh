bb6b58df6e416ea14dd1458a0c474294ed7b306a
mugenjie <85976222+mugenjie@users.noreply.github.com>
2024-09-10 13:03:52 +0800

    Update diy-part1.sh

--- X86/lede/diy-part1.sh
+++ X86/lede/diy-part1.sh
@@ -14,8 +14,9 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-src-git passwall_package https://github.com/xiaorouji/openwrt-passwall-packages
-src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git
-src-git clone https://github.com/sirpdboy/luci-app-ddns-go.git
-src-git OpenClash https://github.com/vernesong/OpenClash.git
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
+echo 'src-git passwall_package https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
+echo 'src-git clone https://github.com/sirpdboy/luci-app-ddns-go.git' >>feeds.conf.default
+echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
